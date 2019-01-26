4866ac5336768de65dc3705b29ed538671670f3c
unknown <christian@ctoglobal.co>
2019-01-26 02:35:22 -0800

    secondcommit

--- src/Board.js
+++ src/Board.js
@@ -7,43 +7,33 @@ class Board extends React.Component {
   }
 
   createBoard() {
-    this.props.matrix
     const board = [];
     let cellCounter = 0;
-    const row = this.props.row;
-    const col = this.props.col;
-    for (let i = 0; i < row; i += 1) {
+    const size = this.props.size;
+    for (let row = 0; row < size; row += 1) {
       const columns = [];
-      for (let j = 0; j < col; j += 1) {
-        columns.push(this.renderSquare(cellCounter++, i, j));
+      for (let col = 0; col < size; col += 1) {
+        const cellValue = this.props.matrix[row][col].move;
+        columns.push(this.renderSquare(cellCounter++, row, col, cellValue));
       }
-      board.push(<div key={i} className="board-row">{columns}</div>);
+      board.push(<div key={row} className="board-row">{columns}</div>);
     }
 
     return board;
   }
 
-  renderSquare(i, row, col) {
-    const winnerClass =
-      this.props.winnerSquares &&
-      (this.props.winnerSquares[0] === i ||
-        this.props.winnerSquares[1] === i ||
-        this.props.winnerSquares[2] === i)
-        ? 'square--green'
-        : '';
-
+  renderSquare(i, row, col, cellValue) {
     return (
       <Square
-        winnerClass={winnerClass}
         key={i}
-        value={this.props.squares[i]}
+        value={cellValue}
         onClick={() => this.props.onClick(row, col)}
       />
     );
   }
 
   render() {
-    return <div>{this.createBoard(3, 3)}</div>;
+    return <div>{this.createBoard()}</div>;
   }
 }
 
--- src/Game.js
+++ src/Game.js
@@ -1,197 +1,118 @@
 import React from 'react';
 import Board from './Board';
 
-const calculateWinner = (squares) => {
-  
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < lines.length; i += 1) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return { winner: squares[a], winnerRow: lines[i] };
-    }
-  }
-
-  return { winner: null, winnerRow: null };
-};
-
-const getLocation = (move) => {
-  const locationMap = {
-    0: 'row: 1, col: 1',
-    1: 'row: 1, col: 2',
-    2: 'row: 1, col: 3',
-    3: 'row: 2, col: 1',
-    4: 'row: 2, col: 2',
-    5: 'row: 2, col: 3',
-    6: 'row: 3, col: 1',
-    7: 'row: 3, col: 2',
-    8: 'row: 3, col: 3',
-  };
-  return locationMap[move];
-};
-
 class Game extends React.Component {
   constructor(props) {
  
     super(props);
-    const row = 5;
-    const col = 5;
+    this.moveValue = "";
     this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null),
-        },
-      ],
-      matrix: this.createBoard(row,col),
-      row: row,
-      col: col,
+      boardSize: 0,
+      matrix: [],
       currentStepNumber: 0,
       xIsNext: true,
+      inputVal: 8
     };
-    
   }
+
  
-  createBoard = (row, col) => {
+  createBoard = () => {
+    const size = this.state.inputVal;
     const board = [];
-    
-    for (let i = 0; i < row; i += 1) {
+    for (let row = 0; row < size; row += 1) {
       board.push([]);
-      for (let j = 0; j < col; j += 1) {
-        board[i].push({
-          col: j,
+      for (let col = 0; col < size; col += 1) {
+        board[row].push({
+          col,
           move: ""
         });
       }
     }
-    return board;
+
+    this.setState({
+      matrix: board,
+      boardSize:size
+    })
   }
 
-  handleClick(row, col, move = "S") {
+  handleMove = (move) => {
+    this.moveValue = move;
+  }
 
-    
-    //find a way to build matrix with updated value
-
-    
-    if(row != 0){
-      const rowTop = row - 1;
-      const rowBottom = row + 1;
-      const colLeft = col - 1;
-      const colRight = col+ 1;
-      let hasPossibleAnswer = false;
-      let myrow= 0;
-      let mycol= 0;
-      do{
-        for(let i=rowTop; i<3; i++){
-          for(let j = colLeft; j< 3; j++){
-            if(move === "S"){
+  handleClick(rowInput, colInput) {
+      let row = rowInput;
+      let col = colInput;
+      const clickedCell = this.state.matrix[row][col];
+      const clickedCellValue = clickedCell.move;
+      if(this.moveValue && !clickedCellValue){
+        let rowTop = (row - 1) < 0 ? 0: row - 1 ;
+        let rowBottom = (row + 1) > this.boardSize ? this.boardSize: row + 1 ;
+        let colLeft = (col - 1) < 0 ? 0: col - 1;
+        const rowTopValue = this.state.matrix[rowTop][col].move;
+        const rowBottomValue = this.state.matrix[rowBottom][col].move;
+        // let chainedMoves = [];
+        // let hasPossibleAnswer = false;
+        // let chain = [];
+        // do{ 
+        //   console.log('start do');
+        //   console.log('rowTop :', rowTop);
+          for(let i=rowTop; i< rowTop + 3; i++){
+            for(let j = colLeft; j< colLeft + 3; j++){
               const cell = this.state.matrix[i][j];
-              if(cell.move === "O"){
-                myrow = i;
-                mycol = j;
-              }
+              const cellValue = cell.move;
+              console.log('cellValue :', cellValue);
             }
           }
-        }
-        hasPossibleAnswer = true;
+        //   hasPossibleAnswer = chain.includes(true);
+        // }
+        // while(hasPossibleAnswer == true)
+       
+        // const cellsWithValues = this.state.matrix.forEach((row, i) => {
+        //   console.log('row :', row);
+        //   console.log('i :', i);
+        // })
+        this.state.matrix[rowInput][colInput].move = this.moveValue;
+        this.setState({
+          matrix: this.state.matrix
+        });
+        // if(this.moveValue == "O" && rowBottomValue == "S" && rowTopValue == "S"){
+        // }
       }
-      while(hasPossibleAnswer == false)
-      console.log('this.state.matrix :', this.state.matrix);
-      this.setState({
-        matrix: this.state.matrix
-      });
-    }
-    
-    // const history = this.state.history.slice(0, this.state.currentStepNumber + 1);
-    // const current = history[history.length - 1];
-    // const squares = current.squares.slice();
 
-    // if (calculateWinner(squares).winner || squares[i]) {
-    //   return;
-    // }
-    // squares[i] = this.state.xIsNext ? 'X' : 'O';
-    // this.setState({
-    //   history: history.concat([
-    //     {
-    //       squares,
-    //       currentLocation: getLocation(i),
-    //       stepNumber: history.length,
-    //     },
-    //   ]),
-    //   xIsNext: !this.state.xIsNext,
-    //   currentStepNumber: history.length,
-    // });
   }
 
-  jumpTo(step) {
-    this.setState({
-      currentStepNumber: step,
-      xIsNext: step % 2 === 0,
-    });
-  }
-
-  sortMoves() {
-    this.setState({
-      history: this.state.history.reverse(),
-    });
+  handleChange = (event) => {
+    this.setState({inputVal: event.target.value});
   }
 
   render() {
-    console.log('this.state.matrix :', this.state.matrix);
-    const { history } = this.state;
-    const current = history[this.state.currentStepNumber];
-    const { winner, winnerRow } = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const currentLocation = step.currentLocation ? `(${step.currentLocation})` : '';
-      const desc = step.stepNumber ? `Go to move #${step.stepNumber}` : 'Go to game start';
-      const classButton = move === this.state.currentStepNumber ? 'button--green' : '';
-
-      return (
-        <li key={step.stepNumber}>
-          <button className={`${classButton} button`} onClick={() => this.jumpTo(move)}>
-            {`${desc} ${currentLocation}`}
-          </button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = `Winner ${winner}`;
-    } else if (history.length === 10) {
-      status = 'Draw. No one won.';
-    } else {
-      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-    }
-
+    console.log('this.state.boardSize :', this.state.boardSize);
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            row={this.state.row}
-            col={this.state.col}
-            matrix={this.state.matrix}
-            squares={current.squares}
-            winnerSquares={winnerRow}
-            onClick={(row, col) => this.handleClick(row,col)}
-          />
+      <div style={{textAlign: "center"}}>
+        <div className={`overlay ${this.state.boardSize == 0 ? "" : "closed"}`}>
+          <div className="content">
+            <h1>Enter Board Size</h1>
+            <input type="text" value={this.state.inputVal} onChange={this.handleChange}/>
+            <button type="button" onClick={() => this.createBoard()}>START</button>
+          </div>
         </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <button className="button" onClick={() => this.sortMoves()}>
-            Sort moves
-          </button>
-          <ol>{moves}</ol>
+        <div className="game">
+          <div className="game-board">
+            {
+              this.state.matrix.length != 0 
+                ? 
+                  <Board
+                    size={this.state.boardSize}
+                    matrix={this.state.matrix}
+                    onClick={(row, col) => this.handleClick(row,col)}
+                  /> 
+                :
+                  ""
+            }
+          </div>
         </div>
+        <button onClick={() => this.handleMove("S")}>S</button>
+        <button onClick={() => this.handleMove("O")}>O</button>
       </div>
     );
   }
